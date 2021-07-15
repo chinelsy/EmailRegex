1867c7a3d2be5b0c332b688ac2817a82b9f1a714
hp <hp@DESKTOP-E81J3KJ>
2021-07-15 12:52:27 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace RegexEmail
+{
+    class Program
+    {
+        //Find all the emails in a string. count them
+        //and print as well.if no email is found, print not found.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any words or sentences of your " +
+           "choice to check for an email match: ");
+            Console.WriteLine();
+            string emailSamples = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            string mails=FindAllEmails(emailSamples);
+            Console.WriteLine();
+            Console.WriteLine(mails);
+            Console.ResetColor();
+            //Console.WriteLine(FindAllEmails(mails));
+            // Console.ReadLine();
+        }
+        public static string FindAllEmails(string emailInput)
+        {
+            string data = (emailInput);
+
+            //instantiate pattern that will match our regex 
+
+            Regex emailRegex = new Regex(@"[a-zA-Z0-9._%+-]+@[a-zA-Z]+(\.[a-zA-Z0-9]+)+",
+            RegexOptions.IgnoreCase);
+
+            //find items that matches with our regex pattern
+            MatchCollection emailMatches = emailRegex.Matches(data);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Match emailMatch in emailMatches)
+            {
+                sb.AppendLine(emailMatch.Value);
+            }
+
+            string emailsRecieved = sb.ToString();
+            return emailsRecieved;
+
+            // return sb.ToString(); OR
+
+
+        }
+    }
+}
